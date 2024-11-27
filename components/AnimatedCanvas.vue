94a35fa7155110f0351659114f5c73f40e2a956b
beanmommo <leonardo.prasetyo5@gmail.com>
2024-11-27 19:43:32 +1100

    Added Trejs and sample component

---
+++ components/AnimatedCanvas.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import * as THREE from 'three'
+</script>
+
+<template>
+    <TresCanvas window-size clear-color="#82DBC5">
+        <TresPerspectiveCamera />
+        <OrbitControls />
+
+        <TresMesh>
+            <TresBoxGeometry />
+            <TresMeshStandardMaterial color="#82DBC5" />
+        </TresMesh>
+
+        <TresLineSegments>
+            <TresEdgesGeometry attach="geometry" :args="[new THREE.BoxGeometry()]" />
+            <TresLineBasicMaterial color="black" :linewidth="5" />
+        </TresLineSegments>
+
+        <TresAmbientLight :intensity="1" />
+    </TresCanvas>
+</template>
---
+++ components/OrbitControls.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+extendTres({ OrbitControls })
+const { camera, renderer } = useTresContext()
+</script>
+
+<template>
+    <TresOrbitControls v-if="renderer" :args="[camera, renderer?.domElement]" />
+</template>
